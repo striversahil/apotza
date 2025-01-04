02cff8f3486ed3e94ecad7f737db2547d1eff877
Sahil Kumar Jha <striversahil@gmail.com>
2025-01-04 15:53:31 +0530

    running setup api

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import cors from "cors";
 const app = express();
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello Wosrldly!");
+  res.status(400).json([{ name: "Alice", msg: "This Message is from API" }]);
 });
 
-app.listen(3005, () => {
+app.listen(4000, () => {
   console.log("Server is running on port 3000");
 });
