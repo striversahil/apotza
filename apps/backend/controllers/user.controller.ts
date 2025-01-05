81e731069287f4659ab8e231b3502c47ce7f14ac
Sahil Kumar Jha <striversahil@gmail.com>
2025-01-05 21:50:48 +0530

    Refactored user creation logic; added JWT support; removed ApiError use

--- apps/backend/controllers/user.controller.ts
+++ apps/backend/controllers/user.controller.ts
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 import ApiResponse from "../helper/ApiResponse";
-import ApiError from "../helper/ApiError";
 import asyncHandler from "../helper/asyncHandler";
 import { User } from "../models/user.model";
 import { Request, Response, NextFunction } from "express";
@@ -10,46 +9,17 @@ const registerUser = asyncHandler(
     const { name, email, password }: any = req.body;
     console.log(req.body);
 
-    throw new ApiError(409, "User with email or username already exists", []);
+    const user = new User({
+      name: name,
+      email: email,
+      password: password,
+    });
 
     await User.create({
       name: name as string,
       email: email as string,
       password: password as string,
     });
-
-    //   if (!Name || !Email || !Password) {
-    //     return res
-    //       .status(400)
-    //       .json(new ApiResponse(400, {}, "All fields are required"));
-    //   }
-    //   const user = await User.findOne({ email: email });
-    //   if (user) {
-    //     return res
-    //       .status(400)
-    //       .json(new ApiResponse(400, {}, "User already exists"));
-    //   }
-
-    // Check if all required fields are provided
-    //     if (!name || !email || !password) {
-    //       return res
-    //         .status(400)
-    //         .json(new ApiResponse(400, {}, "All fields are required"));
-    //     }
-
-    //   const user = new User({
-    //     name: "Sahil",
-    //     email: "email@email.com",
-    //     password: "password",
-    //   });
-
-    //   await user.save();
-
-    //   const createdUser = await User.findOne({ email: "email@email.com" });
-
-    //   if (!createdUser) {
-    //     return res.status(400).json(new ApiResponse(400, {}, "User not created"));
-    //   }
 
     return res.status(201).json(
       new ApiResponse(
--- apps/backend/models/user.model.ts
+++ apps/backend/models/user.model.ts
@@ -1,16 +1,29 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      lowercase: true,
+      trim: true,
       required: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    refreshToken: {
+      type: String,
+      default: "",
     },
     password: {
       type: String,
@@ -20,10 +33,37 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// UserSchema.pre("save", function (next) {});
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-// UserSchema.methods.isCorrectPassword = async function (password) {
-//   return await bcrypt.compare(password, this.password);
-// };
+UserSchema.methods.isCorrectPassword = async function (password: string) {
+  return await bcrypt.compare(this.password as string, password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+UserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", UserSchema);
