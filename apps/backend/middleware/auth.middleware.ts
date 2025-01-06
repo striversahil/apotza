be763fff00dcc9664da125e3a8bff1e0b2493388
Sahil Kumar Jha <striversahil@gmail.com>
2025-01-06 18:23:28 +0530

    Enhanced auth flow, fixed import paths, added redirect on 401

--- apps/backend/middleware/auth.middleware.ts
+++ apps/backend/middleware/auth.middleware.ts
@@ -2,18 +2,32 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import ApiResponse from "../helper/ApiResponse";
 
-const authenticate = (
-  req: Response,
-  res: Response,
-  next: NextFunction
-): any => {
-  const { token } = req.cookie.jwt;
+declare global {
+  namespace Express {
+    interface Request {
+      user: object;
+      password: string;
+    }
+  }
+}
+
+const authenticate = (req: Request, res: Response, next: NextFunction): any => {
+  const token = req.cookies.jwt;
   if (!token) {
     return res
       .status(401)
       .json(new ApiResponse(401, {}, "Redirecting to login..."));
   }
-  return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET as string);
+  // Todo : Verify Token Safely
+  jwt.verify(token, "sahil", (err: any, decoded: any) => {
+    if (err) {
+      return res
+        .status(401)
+        .json(new ApiResponse(401, {}, "Redirecting to login..."));
+    }
+    req.user = decoded;
+  });
+  next();
 };
 
 export default authenticate;
