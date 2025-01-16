37acea6ac3a7d69c55c46ab656bbc45ce55f2958
Sahil Kumar Jha <striversahil@gmail.com>
2025-01-16 16:59:39 +0530

    Updated video and image components styling

--- apps/web/components/Landing/Hero/Brand_Image.tsx
+++ apps/web/components/Landing/Hero/Brand_Image.tsx
@@ -5,17 +5,17 @@ type Props = {};
 
 const Brand_Image = (props: Props) => {
   return (
-    <div className="absolute left-[180px] ">
-      <div className="absolute -top-5 w-full h-10 md:backdrop-blur-lg z-10"></div>
-      <Image
-        src="/brand/hero-dashboard.avif"
-        alt="Vercel Logo"
-        width={1700}
-        height={900}
-        className=""
-      />
-      <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent  rounded-3xl"></div>
-      <div className="absolute -bottom-5 w-full h-10 md:backdrop-blur-md z-10"></div>
+    <div className="absolute left-[180px] rounded-3xl">
+      <div className="w-fit h-fit">
+        <Image
+          src="/brand/hero-dashboard.avif"
+          alt="Vercel Logo"
+          width={1700}
+          height={800}
+          className=""
+        />
+      </div>
+      <div className="absolute  inset-0 bg-gradient-to-r from-slate-950 to-transparent  rounded-3xl"></div>
     </div>
   );
 };
