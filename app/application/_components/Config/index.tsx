18070faae698e48966d04194f061652ee2007989
Sahil Kumar Jha <striversahil@gmail.com>
2025-02-09 09:04:07 +0530

    Add props and simplify div structure in ConfigFolder

--- app/application/_components/Config/index.tsx
+++ app/application/_components/Config/index.tsx
@@ -3,20 +3,20 @@ import { PanelRightClose, PanelRightOpen } from "lucide-react";
 
 type Props = {
   handleOpen: () => void;
+  compData?: any;
+  newData?: (data: any) => void;
 };
 
 const ConfigFolder = ({ handleOpen }: Props) => {
   return (
-    <div className="w-full h-full">
-      <div className={`relative z-10 bg-slate-500  w-full h-full duration-200`}>
-        <div
-          className="absolute z-50 top-2 right-0 p-2 bg-black/50 rounded-md cursor-pointer hover:bg-white/10"
-          onClick={handleOpen}
-        >
-          <PanelRightClose />
-        </div>
-        ConfigFolder
+    <div className="w-full h-full bg-slate-500 ">
+      <div
+        className="absolute z-50 top-2 right-0 p-2 bg-black/50 rounded-md cursor-pointer hover:bg-white/10"
+        onClick={handleOpen}
+      >
+        <PanelRightClose />
       </div>
+      ConfigFolder
     </div>
   );
 };
