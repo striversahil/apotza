55b3a2812ea262282fbb695a46df685a0a612217
Sahil Kumar Jha <striversahil@gmail.com>
2025-02-09 18:15:10 +0530

    Fixed syntax; updated styles; added plugins and data fetching logic

--- app/application/_components/Header/index.tsx
+++ app/application/_components/Header/index.tsx
@@ -1,12 +1,23 @@
+"use client";
+import { getApplicationInfo } from "@actions/user";
 import { Input } from "@components/ui/input";
+import { Skeleton } from "@components/ui/skeleton";
+import { useQueryData } from "@hooks/useQueryData";
 import React from "react";
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const { isLoading, data } = useQueryData("application", getApplicationInfo());
   return (
     <div className="absolute top-0 w-full h-[5vh] bg-slate-900 text-center flex justify-center">
-      <Input value={"Header"} className="w-[20%]" />
+      <h1 className="flex items-center text-3xl text-white font-bold text-center">
+        {isLoading ? (
+          <Skeleton className="w-[500px] h-[40px] rounded-md " />
+        ) : (
+          data.payload.name
+        )}
+      </h1>
     </div>
   );
 };
---
+++ packages/components/Text/config.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@components/ui/input";
+import React from "react";
+
+type Props = {};
+
+const Config = (props: Props) => {
+  return <Input type="text" id="text" name="text" />;
+};
+
+export default Config;
