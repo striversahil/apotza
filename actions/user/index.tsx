37ce5675f593395e3758a0399d8ac8046fcae6d8
Sahil Kumar Jha <striversahil@gmail.com>
2025-02-10 20:54:16 +0530

    Added userLogin and project CRUD actions

--- actions/user/index.tsx
+++ actions/user/index.tsx
@@ -8,8 +8,14 @@ import axios from "axios";
 axios.defaults.withCredentials = true; // Global axios config to enable cookies
 const source = process.env.NEXT_PUBLIC_BASE_URL;
 
+// ++++++++++++++++++++         User Actions                +++++++++++++++++++++++++++++++++++++++
 export const getUserInfo = async () => {
   const response = await axios.get(`${source}/user`);
+  return response.data;
+};
+
+export const userLogin = async (payload: any) => {
+  const response = await axios.post(`${source}/user/signin`, payload);
   return response.data;
 };
 
@@ -18,10 +24,13 @@ export const userSignup = async (payload: any) => {
   return response.data;
 };
 
+// ++++++++++++++++++++++         Workspace Actions                +++++++++++++++++++++++++++++++++++++++
 export const getWorkspaceInfo = async () => {
   const response = await axios.get(`${source}/workspace`);
   return response.data;
 };
+
+// ++++++++++++++++++++++         Project Actions                +++++++++++++++++++++++++++++++++++++++
 
 export const getProjectInfo = async () => {
   const response = await axios.get(`${source}/project`);
@@ -33,7 +42,30 @@ export const newProject = async () => {
   return response.data;
 };
 
-export const userLogin = async (payload: any) => {
-  const response = await axios.post(`${source}/user/signin`, payload);
+// +++++++++++++++++++++++         Project Actions                +++++++++++++++++++++++++++++++++++++++
+
+export const getComponents = async () => {
+  const response = await axios.get(`${source}/project`);
+  return response.data;
+};
+
+export const newComponent = async (payload: any) => {
+  const response = await axios.post(`${source}/project/new`, payload);
+  return response.data;
+};
+
+export const updateComponent = async (payload: any) => {
+  const response = await axios.post(
+    `${source}/project/update/${payload._id}`,
+    payload
+  );
+  return response.data;
+};
+
+export const deleteComponent = async (payload: any) => {
+  const response = await axios.post(
+    `${source}/project/delete/${payload._id}`,
+    payload
+  );
   return response.data;
 };
