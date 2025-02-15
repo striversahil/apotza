4fe7d09e6dbf6dea70d92d36ebd4ac605f0aa4d7
Sahil Kumar Jha <striversahil@gmail.com>
2025-02-15 19:43:38 +0530

    Refactor CodeBlock component, remove openConfig usage

--- app/project/_components/CodeBlock/index.tsx
+++ app/project/_components/CodeBlock/index.tsx
@@ -10,31 +10,33 @@ import { useOpen } from "@app/project/hooks/useOpenCode";
 type Props = {};
 
 const CodeBlock = ({}: Props) => {
-  const { openCode, openConfig, handleOpenCode, handleOpenConfig } = useOpen();
+  const { openCode, handleOpenCode } = useOpen();
 
   return (
-    <Panel
-      defaultSize={40}
-      minSize={20}
-      collapsible
-      onCollapse={handleOpenCode}
-    >
-      {/* Here i Can Minimize the Size of CodeBlock after handle collapsible */}
-      <div className="w-full h-full bg-slate-800">
-        <TabsRoot className="w-full h-full">
-          <Tabs handleOpen={handleOpenCode} />
-          <PanelGroup direction="horizontal">
-            <Panel defaultSize={20} minSize={20} maxSize={50}>
-              <Steps />
-            </Panel>
-            <PanelResizeHandle className="p-[2px] cursor-row-resize hover:bg-blue-500" />
-            <Panel defaultSize={80} minSize={20} maxSize={80}>
-              <EditorCode />
-            </Panel>
-          </PanelGroup>
-        </TabsRoot>
-      </div>
-    </Panel>
+    <>
+      <Tabs handleOpen={handleOpenCode} Open={openCode} />
+      <Panel
+        defaultSize={40}
+        minSize={20}
+        collapsible
+        onCollapse={handleOpenCode}
+      >
+        {/* Here i Can Minimize the Size of CodeBlock after handle collapsible */}
+        <div className="w-full h-full bg-slate-800">
+          <TabsRoot className="w-full h-full">
+            <PanelGroup direction="horizontal">
+              <Panel defaultSize={20} minSize={20} maxSize={50}>
+                <Steps />
+              </Panel>
+              <PanelResizeHandle className="p-[2px] cursor-row-resize hover:bg-blue-500" />
+              <Panel defaultSize={80} minSize={20} maxSize={80}>
+                <EditorCode />
+              </Panel>
+            </PanelGroup>
+          </TabsRoot>
+        </div>
+      </Panel>
+    </>
   );
 };
 
