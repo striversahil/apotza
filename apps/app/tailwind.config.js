7a7555d3366c79f37429e61ad1bb3528e6591f0a
Sahil Kumar Jha <striversahil@gmail.com>
2025-03-13 21:55:10 +0530

    Added dark mode, JakartaSans font, and base styles

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,7 +11,23 @@ module.exports = {
     "../../packages/Layouts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ["JakartaSans", "sans-serif"],
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.850rem",
+        md: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
