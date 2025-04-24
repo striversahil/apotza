3cdbe433777d5ed4cbdf3c89c7dbbb561937d048
Sahil Kumar Jha <striversahil@gmail.com>
2025-04-24 07:42:12 +0530

    feat: implement postgres engine and add error handling to controllers

--- apps/Transformer/Blocs/Rest/index.ts
+++ apps/Transformer/Blocs/Rest/index.ts
@@ -2,32 +2,39 @@ import axios from "axios";
 
 const RestEngine = async (
   endpoint: string,
-  headers: Record<string, string>,
-  body: Record<string, any>
+  headers: Record<string, string>
+  // body: Record<string, any>
 ) => {
-  const response = await axios.get(endpoint, {
-    headers: headers,
-    data: body,
-  });
+  try {
+    const response = await axios.get(endpoint, {
+      headers: headers,
+      // data: body,
+    });
 
-  if (!response) {
+    if (!response) {
+      return {
+        error: "Something went wrong , please try again",
+        data: null,
+      };
+    }
+
+    if (response.status !== 200) {
+      return {
+        error: response.data,
+        data: null,
+      };
+    }
+
     return {
-      error: "Something went wrong , please try again",
+      error: null,
+      data: response.data,
+    };
+  } catch (error) {
+    return {
+      error: error,
       data: null,
     };
   }
-
-  if (response.status !== 200) {
-    return {
-      error: response.data,
-      data: null,
-    };
-  }
-
-  return {
-    error: null,
-    data: response.data,
-  };
 };
 
 export default RestEngine;
--- apps/Transformer/controller/runner.controller.ts
+++ apps/Transformer/controller/runner.controller.ts
@@ -10,68 +10,84 @@ import PythonEngine from "../Blocs/python/index.ts";
 
 class RunnerController {
   static async Rest(c: Context) {
-    let { endpoint, headers, body } = await c.req.json();
+    try {
+      let { endpoint, headers } = await c.req.json();
 
-    if (!endpoint) {
-      return ErrorResponse(c, "Endpoint is required");
-    }
-    if (!headers) {
-      headers = {
-        "Content-Type": "application/json",
-      };
-    }
-    // if (!body) {
-    //   return ErrorResponse(c, "Body is required");
-    // }
+      if (!endpoint) {
+        return ErrorResponse(c, "Endpoint is required");
+      }
+      if (!headers) {
+        headers = {
+          "Content-Type": "application/json",
+        };
+      }
+      // if (!body) {
+      //   return ErrorResponse(c, "Body is required");
+      // }
 
-    const { error, data } = await RestEngine(endpoint, headers, body);
-    if (error) {
-      return ErrorResponse(c, error);
+      const { error, data } = await RestEngine(endpoint, headers);
+      if (error) {
+        return ErrorResponse(c, error);
+      }
+      return SuccessResponse(c, "Success to get data", data);
+    } catch (error) {
+      ErrorResponse(c, "", true);
     }
-    return SuccessResponse(c, "Success to get data", data);
   }
 
   static async Postgres(c: Context) {
-    const { query, connection } = await c.req.json();
+    try {
+      const { query, connection } = await c.req.json();
 
-    if (!query) {
-      return ErrorResponse(c, "Query is required");
-    }
-    if (!connection) {
-      return ErrorResponse(c, "Connection is required");
-    }
+      if (!query) {
+        return ErrorResponse(c, "Query is required");
+      }
+      if (!connection) {
+        return ErrorResponse(c, "Connection Credentials is required");
+      }
 
-    const { error, data } = await PostgresEngine(query, connection);
-    if (error) {
-      return ErrorResponse(c, error);
+      const { error, data } = await PostgresEngine(query, connection);
+      if (error) {
+        return ErrorResponse(c, error);
+      }
+      return SuccessResponse(c, "Success to get data", data);
+    } catch (error) {
+      ErrorResponse(c, "", true);
     }
-    return SuccessResponse(c, "Success to get data", data);
   }
 
   static async JavaScript(c: Context) {
-    const { code } = await c.req.json();
-    if (!code) {
-      return ErrorResponse(c, "Code is required");
-    }
+    try {
+      const { code } = await c.req.json();
+      if (!code) {
+        return ErrorResponse(c, "Code is required");
+      }
 
-    const { error, data } = await JavaScriptEngine(code, null);
-    if (error) {
-      return ErrorResponse(c, error);
+      const { error, data } = await JavaScriptEngine(code, null);
+      if (error) {
+        return ErrorResponse(c, error);
+      }
+      return SuccessResponse(c, "Success to get data", data);
+    } catch (error) {
+      ErrorResponse(c, "", true);
     }
-    return SuccessResponse(c, "Success to get data", data);
   }
 
   static async Python(c: Context) {
-    const { code } = await c.req.json();
-    if (!code) {
-      return ErrorResponse(c, "Code is required");
-    }
+    try {
+      const { code } = await c.req.json();
+      if (!code) {
+        return ErrorResponse(c, "Code is required");
+      }
 
-    const { error, data } = await PythonEngine(code, null);
-    if (error) {
-      return ErrorResponse(c, error);
+      const { error, data } = await PythonEngine(code, null);
+      if (error) {
+        return ErrorResponse(c, error);
+      }
+      return SuccessResponse(c, "Success to get data", data);
+    } catch (error) {
+      ErrorResponse(c, "", true);
     }
-    return SuccessResponse(c, "Success to get data", data);
   }
 }
 
