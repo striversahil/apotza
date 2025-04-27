fb2d173fc6b19ca1991292be6261bc8207dcd3ea
Sahil Kumar Jha <striversahil@gmail.com>
2025-04-27 13:13:17 +0530

    feat: implement user authentication actions for login, signup and logout

--- apps/application/actions/user/user.ts
+++ apps/application/actions/user/user.ts
@@ -1,3 +1,51 @@
+import { useMutationData } from "@/hooks/useMutation";
+import api from "..";
+
+const source = "/user";
+
 export const UserAction = {
-  login: () => {},
+  login: () => {
+    const { mutate } = useMutationData(
+      ["user-login"],
+      async (payload: any) => {
+        const response = await api.post(`${source}/signin`, payload);
+        return response.data;
+      },
+      [["userinfo"]]
+      // () => {
+      //   console.log("User updated successfully!");
+      // }
+    );
+    return { mutate };
+  },
+
+  signup: () => {
+    const { mutate } = useMutationData(
+      ["user-signup"],
+      async (payload: any) => {
+        const response = await api.post(`${source}/signup`, payload);
+        return response.data;
+      },
+      [["userinfo"]]
+      // () => {
+      //   console.log("User updated successfully!");
+      // }
+    );
+    return { mutate };
+  },
+
+  logout: () => {
+    const { mutate } = useMutationData(
+      ["user-logout"],
+      async (payload: any) => {
+        const response = await api.post(`${source}/logout`, payload);
+        return response.data;
+      },
+      [["userinfo"]]
+      // () => {
+      //   console.log("User updated successfully!");
+      // }
+    );
+    return { mutate };
+  },
 };
