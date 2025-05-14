8de80dceaa3f2b5e2dc3a70d771e0bbe604178f0
Sahil Kumar Jha <striversahil@gmail.com>
2025-05-14 18:16:37 +0530

    feat: Add preview pages with layout and project data handling

---
+++ apps/application/app/preview/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import GetProject from "@/actions/project";
+import { Skeleton } from "@repo/ui/skeleton";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  const { isLoading, data } = GetProject.getProject();
+
+  if (isLoading) {
+    return (
+      <div>
+        <Skeleton className="w-screen h-screen rounded-md " />
+      </div>
+    );
+  }
+
+  if (data && data.success === false) {
+    redirect("/login");
+  }
+
+  if (data && data.success === true) {
+    redirect(`/application/${data.payload.id}/edit`);
+  }
+
+  return (
+    <div>
+      <div>Project</div>
+      {JSON.stringify(data)}
+    </div>
+  );
+};
+
+export default page;
